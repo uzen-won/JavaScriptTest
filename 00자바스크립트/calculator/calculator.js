379c20e9acb4b511576f48518631663c1c8faaec
won <won@uzen.net>
2023-06-23 17:49:43 +0900

    계산기

--- 00자바스크립트/calculator/calculator.js
+++ 00자바스크립트/calculator/calculator.js
@@ -1,22 +1,44 @@
 /* 키보드 이벤트 */
 
-/* window.addEventListener("keydown", e => {
-        const key = e.key;
-        const keyId = `#b${key}`;
-        const keyElement = document.querySelector(keyId);
- 
- 
-        keyElement.addEventListener("click", () => {
-        // 클릭 이벤트가 발생했을 때 수행할 동작을 여기에 작성하세요.
-        // 예를 들어, 클릭되었을 때 어떤 동작을 수행하거나 함수를 호출할 수 있습니다.
-        console.log("클릭되었습니다!");
-      });
-      }); */
 window.addEventListener("keydown", (e) => {
   console.log(e);
   const key = e.key;
-  const keyId = `#btn${key}`;
+  console.log(key)
+  let keyId =""
+  switch (key) {
+    case "+":
+      keyId = "#plus";
+      break;
+    case "-":
+      keyId = "#minus";
+      break;
+    case "Enter":
+      keyId = "#eq";
+      break;
+    case ".":
+      keyId = "#dot";
+      break;
+    case "/":
+      keyId = "#divide";
+      break;
+    case "C":
+      keyId = "#reset";
+      break;
+    case "*":
+      keyId = "#multiply";
+      break;
+    case "Backspace":
+      keyId = "#del";
+      break;
+    default: //변수 != 상수1 이고, 변수 != 상수2 이면, 실행문 C 실행
+      keyId = `#btn${key}`;
+  }
+
+
+//keyId = `#btn${key}`;
   const keyElement = document.querySelector(keyId);
+
+  //reset divide multiply del plus minus dot eq
 
   if (keyElement) {
     // keyElement를 클릭하도록 강제로 이벤트를 발생시킴
@@ -29,12 +51,6 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
-/* 
-    숫자 누르면 첫번째변수에 저장.
-    기호 버튼을 누르고 변수에 기호를 저장하고 숫자를 누르면 두번째 변수에 저장.
-    = 누르면 세번쨰 변수에 => 첫번쨰변수 기호 두번째 변수 계산 => 결과창에 출력.
-    결과창에 출력하고 세번째 변수를 첫번째 변수에 넣는다.
-    */
 window.onload = function () {
   let num = document.querySelectorAll(".num");
   let cal = document.querySelectorAll(".cal");
@@ -45,6 +61,8 @@ window.onload = function () {
   let firstNum = document.querySelector("#firstNum");
   let symbol = document.querySelector("#symbol");
   let lastNum = document.querySelector("#lastNum");
+  let procedure = document.querySelector(".procedure");
+  let resultBox = document.querySelector(".result-box");
   let numValue1 = "";
   let calValue = "";
   let numValue2 = "";
@@ -60,6 +78,10 @@ window.onload = function () {
       calValue = e.target.innerText;
       //console.log(calValue);
       symbol.textContent = calValue;
+
+
+      procedure.style.display = "block";
+      resultBox.style.display = "none";
     });
   });
 
@@ -80,6 +102,8 @@ window.onload = function () {
         lastNum.textContent = numValue2;
         //console.log('두번쨰 값 : ' + numValue2);
       }
+      procedure.style.display = "block";
+      resultBox.style.display = "none";
     });
   });
 
@@ -110,19 +134,26 @@ window.onload = function () {
     }
     result.textContent = funcResult;
 
+
+
     //history
-    let resultHistory = document.createElement("div");
-    resultHistory.append(
-      `${resultCount}. ` +
-        firstNum.textContent +
-        symbol.textContent +
-        lastNum.textContent +
-        "=" +
-        result.textContent
-    );
-    resultCount++;
-    console.log(resultHistory);
-    let insertHtml = document.querySelector(".history").prepend(resultHistory);
+    if (firstNum.textContent && symbol.textContent && lastNum.textContent) {
+      let resultHistory = document.createElement("div");
+      resultHistory.append(
+        `${resultCount}. ` +
+          firstNum.textContent +
+          symbol.textContent +
+          lastNum.textContent +
+          "=" +
+          result.textContent
+      );
+      resultCount++;
+      console.log(resultHistory);
+      let insertHtml = document
+        .querySelector(".history")
+        .prepend(resultHistory);
+    }
+
     //historyBtn
     const historyBtn = document.querySelector("#historyBtn");
     const historyBox = document.querySelector(".history-box");
@@ -137,6 +168,9 @@ window.onload = function () {
       calValue = "";
       spanEmpty.textContent = ""; // 보이는것 빈칸
     });
+
+    procedure.style.display = "none";
+    resultBox.style.display = "block";
   });
 
   reset.addEventListener("click", function () {
